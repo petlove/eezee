b89f89d93943bc7476315240a8c22addfbcfa226
JP <jao@users.noreply.github.com>
2024-10-21 18:42:41 -0300

    chore: bumps rexml version (#11)
    * chore: bumps rexml version
    
    * chore: bumps gem versions
    
    * chore: adds codeowners
    
    * chore: adds cobertura.xml generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,12 +10,24 @@ require 'support/shared/eezee/client/requester/post'
 require 'support/shared/eezee/client/requester/patch'
 require 'support/shared/eezee/client/requester/put'
 require 'support/shared/eezee/client/requester/delete'
+require 'simplecov-cobertura'
+require 'simplecov_json_formatter'
 require 'pry'
 
 SimpleCovConfig.configure
 VCRConfig.configure
 
 require 'eezee'
+
+SimpleCov.start do
+  enable_coverage :branch
+end
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::JSONFormatter,
+  SimpleCov::Formatter::CoberturaFormatter
+]
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
